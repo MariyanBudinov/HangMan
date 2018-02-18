5105e75340252c4bcb1d68f029a0f9d9c220d8cd
MariyanBudinov <mariqn_b@live.com>
2018-02-18 22:15:43 +0200

    update: add guess buttons and functionality.

--- app/components/HiddenWordBuilder.js
+++ app/components/HiddenWordBuilder.js
@@ -14,7 +14,8 @@ class HiddenWordBuilder {
      */
     buildSimbolBox(insertedText, input) {
         let inputWord = this._validate(insertedText),
-            hiddenPanel = document.querySelector('.hidden-panel');
+            hiddenPanel = document.querySelector('.hidden-panel'),
+            guessBox = document.querySelector('.guess-box');
 
         if (!inputWord || inputWord.length === 0) {
             alert(`Please type some word!`);
@@ -22,14 +23,22 @@ class HiddenWordBuilder {
             return;
         }
 
+        guessBox.classList.toggle('disabled', false);
+        input.classList.toggle('disabled', true);
+
+        this._loadExitButton(guessBox, input);
+        this._loadGuessButton(inputWord);
+
         inputWord.forEach(letter => {
             let box = document.createElement('div'),
                 text = document.createElement('h2');
+            text.style.backgroundColor = 'white';
             text.innerHTML = letter;
+            text.className = 'signs';
             box.className = 'hidden-box';
             box.appendChild(text);
             hiddenPanel.appendChild(box);
-        })
+        });
     }
 
     /**
@@ -51,6 +60,43 @@ class HiddenWordBuilder {
         } else {
             return textArr.map(sign => sign.toUpperCase());
         }
+    }
+
+    _loadExitButton(guessBox, input) {
+        let exitButton = document.querySelector('.exit-button');
+        exitButton.addEventListener('click', (event) => {
+            if (confirm('Are you shure ?')) {
+                event.preventDefault();
+                guessBox.classList.toggle('disabled', true);
+                input.classList.toggle('disabled', false);
+                input.focus();
+            }
+        })
+    }
+
+    _loadGuessButton(inputWord) {
+        let guessButton = document.querySelector('.guess-button');
+        guessButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            let guessInput = document.querySelector('.guess-input'),
+                allSigns = document.querySelectorAll('.signs'),
+                guessLetter = guessInput.value.toUpperCase();
+            if (guessLetter.length !== 1) {
+                return alert('No no no, you can only guess one by one letters in word !');
+            }
+            if (!inputWord.includes(guessLetter.toUpperCase())) {
+                guessInput.value = '';
+                guessInput.focus();
+                return alert('WRONG !');
+            }
+            allSigns.forEach(signElement => {
+                if (signElement.textContent === guessLetter) {
+                    guessInput.value = '';
+                    guessInput.focus();
+                    signElement.style.backgroundColor = 'green';
+                }
+            });
+        })
     }
 
 }
